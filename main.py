e42b0c278a548e49ef0c3731ea192a1272199809
M1nten <minna.soe@specialisterne.dk>
2026-09-17 14:23:09 +0200

    H: opdater read-funktion til at vælge csv-filer

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-def main() -> None:
-    file_path = "csv_filer/employees.ascii.csv"
-    csv_string = get_csv_file(file_path)
-    print(csv_string)
-
+import sys
 
 def get_csv_file(path: str) -> str:
     with open(path) as f:
-        return f.read()
+        csv_file = f.read()
+        return csv_file
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_file>")
+        sys.exit(1)
+    file_path = sys.argv[1]
+    #file_path = "csv_filer/employees.ascii.csv"
+    csv_string = get_csv_file(file_path)
+    print(repr(csv_string))
 
 
 main()
-
-
-
-#  def main() -> None:
-#     book_path = "books/frankenstein.txt"
-#     text = get_book_text(book_path)
-#     print(text)
-
-
-# def get_book_text(path: str) -> str:
-#     with open(path) as f:
-#         return f.read()
